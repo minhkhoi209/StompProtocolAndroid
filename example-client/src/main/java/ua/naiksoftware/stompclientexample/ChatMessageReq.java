4a221c5104862af475629fd3b598945b30d5fa45
Khoi Nguyen <khoi@minhkhoi.dev>
2021-11-18 00:44:01 +0700

    Fix

--- example-client/src/main/java/ua/naiksoftware/stompclientexample/ChatMessageReq.java
+++ example-client/src/main/java/ua/naiksoftware/stompclientexample/ChatMessageReq.java
@@ -5,28 +5,7 @@ package ua.naiksoftware.stompclientexample;
  * Created by Naik on 24.02.17.
  */
 public class ChatMessageReq {
-
-    private String sender;
-    private String timeSend;
     private String message;
-    private String linkImage;
-    private String linkVideo;
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public String getTimeSend() {
-        return timeSend;
-    }
-
-    public void setTimeSend(String timeSend) {
-        this.timeSend = timeSend;
-    }
 
     public String getMessage() {
         return message;
@@ -34,21 +13,5 @@ public class ChatMessageReq {
 
     public void setMessage(String message) {
         this.message = message;
-    }
-
-    public String getLinkImage() {
-        return linkImage;
-    }
-
-    public void setLinkImage(String linkImage) {
-        this.linkImage = linkImage;
-    }
-
-    public String getLinkVideo() {
-        return linkVideo;
-    }
-
-    public void setLinkVideo(String linkVideo) {
-        this.linkVideo = linkVideo;
     }
 }
--- example-client/src/main/java/ua/naiksoftware/stompclientexample/MainActivity.java
+++ example-client/src/main/java/ua/naiksoftware/stompclientexample/MainActivity.java
@@ -24,19 +24,23 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.CompletableTransformer;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.internal.fuseable.ScalarSupplier;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import ua.naiksoftware.stomp.Stomp;
 import ua.naiksoftware.stomp.StompClient;
 import ua.naiksoftware.stomp.dto.StompHeader;
+import ua.naiksoftware.stomp.dto.StompMessage;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private static final String WS_URI = "ws://10.0.2.2:8080/ws";
     private static final String AUTH_HEADER = "Authorization";
-    private static final String AUTH_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjM1ODY2NDM5LCJleHAiOjE2MzY0NzEyMzl9.WUmAGHbIfnVT9lPDvL4h7YiYt5hBW4pFVKSzyfc7BVwmT3OVEG9Y1YX8pnL09cyMGR66So3Fn4x26xIFCgzQOA";
-    private static final String ROOM_CHANNEL = "/chat/room/60/message";
-    private static final String SEND_CHANNEL = "/app/chat/60/send";
+    private static final String AUTH_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMCIsImlhdCI6MTYzNjkwOTcyMCwiZXhwIjoxNjM3NTE0NTIwfQ.YnxTB8F6spESHWdLBeSMnft9t6bqB3z8YS9BqW_OmlOWXJint-vprwSYVFBVajKli3J8RgiD_ROib9NaAC3MDA";
+    private static final String ROOM_CHANNEL = "/chat/20/messages";
+    private static final String USER_CHANNEL = "/user/10/queue/messages";
+    private static final String SEND_CHANNEL = "/app/chat/20/send";
 
     private final Gson mGson = new GsonBuilder().create();
     private final List<String> mDataSet = new ArrayList<>();
@@ -112,25 +116,30 @@ public class MainActivity extends AppCompatActivity {
 
         compositeDisposable.add(dispLifecycle);
 
+        Consumer<StompMessage> messageReceivedConsumer = topicMessage -> {
+            Log.d(TAG, "Received " + topicMessage.getPayload());
+            topicMessage.getStompHeaders().forEach(stompHeader -> toast("Header[" + stompHeader.getKey()+"]=" + stompHeader.getValue()));
+            if (Boolean.parseBoolean(topicMessage.findHeader("success"))) {
+                toast("Success Message payload = " + topicMessage.getPayload());
+            }
+
+        };
+//        // Receive greetings
+//        Disposable dispTopic = mStompClient.topic(editMsgChannel.getText().toString())
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(messageReceivedConsumer, throwable -> Log.e(TAG, "Error on subscribe topic", throwable));
+
         // Receive greetings
-        Disposable dispTopic = mStompClient.topic(editMsgChannel.getText().toString())
+        Disposable userChannel = mStompClient.topic(USER_CHANNEL)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(topicMessage -> {
-                    Log.d(TAG, "Received " + topicMessage.getPayload());
-                    WsResponse<ChatMessageRes> wsResponse = convertWsResponse(
-                            topicMessage.getPayload(),
-                            ChatMessageRes.class);
-                    if (wsResponse.isSuccess()) {
-                        addItem(wsResponse.getData());
-                        return;
-                    }
+                .subscribe(messageReceivedConsumer, throwable -> Log.e(TAG, "Error on subscribe topic", throwable));
 
-                    toast(wsResponse.getMessage());
 
-                }, throwable -> Log.e(TAG, "Error on subscribe topic", throwable));
 
-        compositeDisposable.add(dispTopic);
+//        compositeDisposable.add(dispTopic);
+        compositeDisposable.add(userChannel);
         mStompClient.connect(Collections.singletonList(new StompHeader(AUTH_HEADER, AUTH_TOKEN)));
     }
 
@@ -139,9 +148,7 @@ public class MainActivity extends AppCompatActivity {
         if (mStompClient == null || !mStompClient.isConnected()) return;
 
         ChatMessageReq msg = new ChatMessageReq();
-        msg.setSender("Android");
         msg.setMessage(editMsgInput.getText().toString());
-        msg.setTimeSend(Instant.now().toString());
 
 
         compositeDisposable.add(mStompClient.send(editRoomChannel.getText().toString(), mGson.toJson(msg))
